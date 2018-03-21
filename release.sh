7b64d95e959b9ed7e2db7d9332276795cb56d4f7
Oliver Kopp <kopp.dev@gmail.com>
2018-03-21 17:59:52 +0100

    Fix creation of stripped-down README.md on CircleCI

--- release.sh
+++ release.sh
@@ -6,8 +6,9 @@ sed -i "s/# plantuml.*/# plantuml/" README.md
 pandoc README.md --pdf-engine=lualatex -o plantuml.pdf
 
 # Keep first paragraph of README.md only
-# hint for multiline matching from https://unix.stackexchange.com/a/369899/18033
-cat README.md | awk 1 ORS=__ABC__ | sed -e "s/__ABC__## Preconditions.*//" | awk 1 RS=__ABC__ > README.md
+# Hint for multiline matching from https://unix.stackexchange.com/a/369899/18033
+# We have to use tmp.md, because of file locking on CircleCI
+cat README.md | awk 1 ORS=__ABC__ | sed -e "s/__ABC__## Preconditions.*//" | awk 1 RS=__ABC__ > tmp.md && mv tmp.md README.md
 
 # Prepare for CTAN
 ctanify --notds plantuml.sty plantuml.lua plantuml.pdf README.md CHANGELOG.md "example-*.tex"

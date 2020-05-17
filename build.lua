ef025ee2a2d7ee14c86a0f79249fb4ef2e13ad85
Jeff Tian <Jeff.Tian@externals.adidas.com>
2020-05-17 15:15:31 +0800

    ci: use l3build to upload to CTAN

---
+++ build.lua
@@ -0,0 +1,11 @@
+uploadconfig = {
+  pkg          = "plant-uml",
+  version      = "0.3.3",
+  author       = "Jeff Tian",
+  uploader     = "Jeff Tian",
+  license      = "lppl1.3c",
+  summary      = "A LuaLaTeX package for PlantUML in LaTeX",
+  ctanPath     = "/graphics/plantuml",
+  repository   = "https://github.com/jeff-tian/plantuml/",
+  note         = [[Uploaded automatically by l3build...]]
+}
